185e7fe38ed2f318013e93d96ffeebcc94e7414d
Peyton Runyan <prunyan@recyclingpartnership.org>
2021-11-04 05:08:28 -0400

    add comments for for methods and functions

--- internal/server/http.go
+++ internal/server/http.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// wraps our log httpServer in an http.Server with handlers registered
 func NewHTTPServer(addr string) *http.Server {
 	httpServer := newHTTPServer()
 	r := mux.NewRouter()
@@ -18,10 +19,12 @@ func NewHTTPServer(addr string) *http.Server {
 	}
 }
 
+// struct to hold our log and our handler methods
 type httpServer struct {
 	Log *Log
 }
 
+// don't confuse this with http.Server
 func newHTTPServer() *httpServer {
 	return &httpServer{
 		Log: NewLog(),
@@ -44,39 +47,41 @@ type ConsumeResponse struct {
 	Record Record `json:"record"`
 }
 
+// unmarshalls request, appeds message to the log, returns offset
 func (s *httpServer) handleProduce(w http.ResponseWriter, r *http.Request) {
 	var req ProduceRequest
-	err := json.NewDecoder(r.Body).Decode(&req)
+	err := json.NewDecoder(r.Body).Decode(&req) // unmarshall
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	off, err := s.Log.Append(req.Record)
+	off, err := s.Log.Append(req.Record) // append to log
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	res := ProduceResponse{Offset: off}
-	err = json.NewEncoder(w).Encode(res)
+	resp := ProduceResponse{Offset: off}
+	err = json.NewEncoder(w).Encode(resp) // return offset
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 }
 
+// unmarshalls request, finds record at given offset, returns record
 func (s *httpServer) handleConsume(w http.ResponseWriter, r *http.Request) {
 	var req ConsumeRequest
-	err := json.NewDecoder(r.Body).Decode(&req)
+	err := json.NewDecoder(r.Body).Decode(&req) // unmarshall
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	record, err := s.Log.Read(req.Offset)
+	record, err := s.Log.Read(req.Offset) // find record
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	err = json.NewEncoder(w).Encode(record)
+	err = json.NewEncoder(w).Encode(record) // return record
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
